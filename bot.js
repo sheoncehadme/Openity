27248ca4593e9897bf9c80945222c2c4bf6b4ab9
Evan Brown <noggin@gmail.com>
2015-03-23 17:40:55 -0400

    Added bunyan logger and prepping for redis database ability

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 #!/usr/bin/nodejs
 
 var irc = require('irc');
-var config = require('./config');
+global.config = require('./config');
 var handler = require('./include/handler.js');
 
-var client = new irc.Client(config.server, config.nick, config.options);
-handler.bind(client, config);
+var client = new irc.Client(global.config.server, global.config.nick, global.config.options);
+handler.bind(client);
--- include/handler.js
+++ include/handler.js
@@ -1,21 +1,40 @@
 var twss = require('twss');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+    name: global.config.nick,
+    streams: [
+        {
+            level:'info',
+            stream: process.stdout
+        },
+        {
+            level: 'info',
+            path: './'+global.config.nick+'.log'
+        }
+    ]
+});
+
 twss.threshold = 0.999;
 
-var bind = function(client, config) {
+var bind = function(client) {
     client.addListener('message', function (from, to, message) {
         if(twss.is(message)){
-          client.say(to, 'That\'s what she said!');
+          client.say(to, "That's what she said!");
         }
-        console.log(from + ' => ' + to + ': ' + message);
-        console.log('TWSS probablility: '+twss.probability(message));
+        log.info({from: from,
+            to: to,
+            message: message,
+            twssprobability: twss.probability(message)
+        });
     });
 
     client.addListener('error', function(message) {
-        console.log('error: ', message);
+        log.error( message);
     });
 
     client.addListener('pm', function(from, message){
-        if((config.admins.indexOf(from) !== -1) && message == 'quit'){
+        if((global.config.admins.indexOf(from) !== -1) && message == 'shutdown'){
+           log.info({from: from, message: 'Shutdown requested'});
            client.disconnect();
         }
     });
